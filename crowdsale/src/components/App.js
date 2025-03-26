ea79c1cee53719f735084f7f08acdeaa9f808d36
cplKlegg <emcgrath@startmail.com>
2025-03-26 19:36:34 -0400

    React-bootstrap skeleton front end

--- crowdsale/src/components/App.js
+++ crowdsale/src/components/App.js
@@ -1,8 +1,12 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function App() {
-	return(
-		<div>Hello, world!</div>
-	)
+	//return(
+
+	//)
 }
 
 export default App;
