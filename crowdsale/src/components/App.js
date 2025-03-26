f2c7a8d7644d2735faab182173e9e986902d46cf
cplKlegg <emcgrath@startmail.com>
2025-03-26 19:47:43 -0400

    Navbar smoketest

--- crowdsale/src/components/App.js
+++ crowdsale/src/components/App.js
@@ -1,12 +1,11 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Container } from 'react-bootstrap';
 
 function App() {
-	//return(
-
-	//)
+	return(
+		<Container>
+			<h1>Hello, world!</h1>
+		</Container>
+	)
 }
 
 export default App;
