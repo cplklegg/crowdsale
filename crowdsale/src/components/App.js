6a8f290b4457228a437d321a66e007b5f05f0ec3
cplKlegg <emcgrath@startmail.com>
2025-03-28 16:13:09 -0400

    initial issues with Metamask coonect o project

--- crowdsale/src/components/App.js
+++ crowdsale/src/components/App.js
@@ -1,9 +1,21 @@
+import { useEffect } from 'react';
 import { Container } from 'react-bootstrap';
+import { ethers } from 'ethers';
 
 //Components 
 import Navigation from './Navigation';
 
 function App() {
+	
+	const loadBlockchainData = async () => {
+		const provider = new ethers.providers.Web3Provider(window.ethereum)
+		console.log(provider)
+	}
+
+	useEffect(() => {
+		loadBlockchainData()
+	});
+
 	return(
 		<Container>
 			<Navigation />
