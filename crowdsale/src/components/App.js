6e6a1e306101cb77fb40bde6190aeb2601f6fbaa
cplKlegg <emcgrath@startmail.com>
2025-04-02 20:58:16 -0400

    page renders!

--- crowdsale/src/components/App.js
+++ crowdsale/src/components/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
 import { ethers } from 'ethers';
 
@@ -6,10 +6,24 @@ import { ethers } from 'ethers';
 import Navigation from './Navigation';
 
 function App() {
+
+	const [account, setAccount] = useState(null);
+
+	// account -> Variable of current account value
+	// setAccount('0x0...') -> Function to update account value
+	// null is a default value
 	
 	const loadBlockchainData = async () => {
 		const provider = new ethers.providers.Web3Provider(window.ethereum)
 		console.log(provider)
+
+		const accounts = await window.ethereum.request({ method: 'eth_requestAccounts'})
+		const account = ethers.utils.getAddress(accounts[0])
+		console.log(account)
+		setAccount(account)
+
+
+		// Add to state
 	}
 
 	useEffect(() => {
@@ -19,6 +33,8 @@ function App() {
 	return(
 		<Container>
 			<Navigation />
+			{/* { Read from state} */}
+			<div>{account}</div>
 		</Container>
 	)
 }
