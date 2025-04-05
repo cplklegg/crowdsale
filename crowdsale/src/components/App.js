d6e7fbf97f1a5c4bf387ec23573f04854e379b99
cplKlegg <emcgrath@startmail.com>
2025-04-05 13:37:05 -0400

    issues with acct balance loading

--- crowdsale/src/components/App.js
+++ crowdsale/src/components/App.js
@@ -6,31 +6,54 @@ import { ethers } from 'ethers';
 import Navigation from './Navigation';
 import Info from './Info';
 
-function App() {
-	const [provider, setProvider] = useState(null);
-	const [account, setAccount] = useState(null);
+// ABIs
+import TOKEN_ABI from '../abis/Token.json'
+import CROWDSALE_ABI from '../abis/Crowdsale.json'
 
-	// account -> Variable of current account value
-	// setAccount('0x0...') -> Function to update account value
-	// null is a default value
-	
+//config
+import config from '../config.json';
+
+function App() {
+	const [provider, setProvider] = useState(null)
+	const [crowdsale, setCrowdsale] = useState(null)
+
+	const [account, setAccount] = useState(null)
+
+	const [isLoading, setIsLoading] = useState(true)
+
 	const loadBlockchainData = async () => {
 		// Initiate provider
 		const provider = new ethers.providers.Web3Provider(window.ethereum)
 		setProvider(provider)
 
+		// Fetch Chain ID
+		const { chainId } = await provider.getNetwork()
+
+		// Initiate contracts
+		const token = new ethers.Contract(config[chainId].token.address, TOKEN_ABI , provider)
+		const crowdsale = new ethers.Contract(config[chainId].crowdsale.address, CROWDSALE_ABI , provider)
+		setCrowdsale(crowdsale)
+
+		// Fetch accounts
 		const accounts = await window.ethereum.request({ method: 'eth_requestAccounts'})
 		const account = ethers.utils.getAddress(accounts[0])
-		
 		setAccount(account)
 
+		// Fetch account balance
+		const accountBalance = await token.balanceOf(account)
+		console.log(accountBalance)
+
+
+		setIsLoading(false)
 
 		// Add to state
 	}
 
 	useEffect(() => {
-		loadBlockchainData()
-	});
+		if (isLoading) {
+			loadBlockchainData()
+		}		
+	}, [isLoading]);
 
 	return(
 		<Container>
